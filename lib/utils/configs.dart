1a7ed45bc8bd2e66bc46968e76e4d045843f16f2
DagiABAY <dagimawulachew@gmail.com>
2024-03-19 14:50:05 +0300

    Finalized MovieMagic app: refined UI, optimized performance. A seamless movie-finding experience awaits

--- lib/utils/configs.dart
+++ lib/utils/configs.dart
@@ -3,19 +3,10 @@ import 'package:flutter/services.dart';
 
 class Config {
   Config();
-  static String baseApiUrl="https://api.themoviedb.org/3";
+  static String baseApiUrl="YOUR_API_URL";
   static String? apiKey;
-  static String? baseImageApiUrl="https://image.tmdb.org/t/p/w500";
-  static String token="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjExOThmNDlmNDdlMDI2YTM4NGIzY2UwYjcyZDFiNCIsInN1YiI6IjY1ZjI5Nzk2ZWVhMzRkMDE4ODE1ZjI5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._llMvlHCJtfaBKmgDNdbVk7mgDn82Ur17psq53oYClw";
-  // static setUp() async {
-  //   final configFile = await rootBundle.loadString("assets/configs/main.json");
-  //   final configData = jsonDecode(configFile);
-  //   apiKey = configData['API_KEY'];
-  //   print("keyyyyyyyyyyyyyyyy$apiKey");
-  //   baseApiUrl = configData['BASE_API_URL'];
-  //   baseImageApiUrl = configData['BASE_IMAGE_API_URL'];
-  //   token = configData['token'];
-  // }
+  static String? baseImageApiUrl="YOUR_IMAGE_PATH";
+  static String token="YOUR_TOKEN";
 
 
 }
